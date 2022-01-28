9222ebc6c434c6e10a450bc04418f2317311f897
Fabricio Lima <fabriciolc61@gmail.com>
2022-01-28 12:28:18 -0300

    criando hook useRoom

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -2,11 +2,11 @@ import { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
 import { useRouter } from "next/router"
-import { FormEvent, useEffect, useState } from "react"
+import { FormEvent, useState } from "react"
 import logo from '../../assets/images/logo.svg'
-import Button from "../../components/Button"
-import RoomCode from "../../components/RoomCode"
+import { Button, Question, RoomCode } from '../../components/index'
 import { useAuth } from "../../context/auth.context"
+import useRoom from "../../hooks/useRoom"
 import { database } from "../../services/firebase"
 import 
 {   Container, 
@@ -14,64 +14,18 @@ import
     FooterForm, 
     Forms, 
     Header, 
+    QuestionList, 
     UserInfo, 
     Wrapper 
 } from "./styles"
 
-type FirebaseQuestions = Record<string, {
-    author: {
-        name: string
-        avatar: string
-    },
-    content: string
-    isAnswered: boolean
-    isHighlighted: boolean
-}>
-
-type Questions = {
-    id: string    
-    author: {
-        name: string
-        avatar: string
-    },
-    content: string
-    isAnswered: boolean
-    isHighlighted: boolean
-}
-
 const Room: NextPage = () => {
+    
     const router = useRouter()
+    const roomId = router.query.id
     const { user } = useAuth()
-
-    const { id } = router.query
-
+    const { questions, title } = useRoom(String (roomId))
     const [ newQuestion, setNewQuestion ] = useState('')
-    const [ questions, setQuestions ] = useState<Questions[]>([])
-    const [ title, setTitle ] = useState('')
-
-    useEffect(() => {
-        const roomRef = database.ref(`rooms/${id}`)
-
-        roomRef.on('value', room => {
-            const databaseRoom = room.val()
-
-            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}
-
-            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
-                return {
-                    id: key,
-                    content: value.content,
-                    author: value.author,
-                    isHighlighted: value.isHighlighted,
-                    isAnswered: value.isAnswered
-                }
-            })
-            
-            setTitle(databaseRoom.title)
-            setQuestions(parsedQuestions)
-        })
-    }, [id])
-
 
     const handleSendQuestion = async (event: FormEvent) => {
         event.preventDefault()
@@ -94,11 +48,10 @@ const Room: NextPage = () => {
             isAnswered: false
         }
 
-        await database.ref(`rooms/${id}/questions`).push(question)
+        await database.ref(`rooms/${roomId}/questions`).push(question)
 
         setNewQuestion('')
     }
-    
 
     return (
         <>
@@ -106,63 +59,72 @@ const Room: NextPage = () => {
                 <title> Room </title>
             </Head> 
 
-            <div>
-                <Header>
-                    <Content>
-                        <Image 
-                            src={logo} 
-                            alt='letmeask' 
-                            height={45}
-                        />
-                        <RoomCode code={ String(id) }/>
-                    </Content>
-                </Header>
+            <Header>
+                <Content>
+                    <Image 
+                        src={logo} 
+                        alt='letmeask' 
+                        height={45}
+                    />
+                    <RoomCode code={ String(roomId) }/>
+                </Content>
+            </Header>
 
-                <Wrapper>
-                    <Container>
-                        <h1> Sala {title} </h1>
-                        {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
-                    </Container>
+            <Wrapper>
+                <Container>
+                    <h1> Sala {title} </h1>
+                    {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+                </Container>
 
-                    <Forms onSubmit={handleSendQuestion}>
-                        <textarea 
-                            placeholder='O que você quer perguntar? '
-                            value={newQuestion}
-                            onChange={event => setNewQuestion(event.target.value)}
-                        />
-                        <FooterForm>
-                            { user ? 
-                                (
-                                    <UserInfo>
-                                        <div style={{borderRadius: '50%', overflow: 'hidden'}}>
-                                            <Image 
-                                                src={user.avatar} 
-                                                alt={user.name}
-                                                width={32}
-                                                height={32}
-                                            />
-                                        </div>
-                                        <span>{user.name}</span>
-                                    </UserInfo>
-                                ) : 
-                                (
-                                    <span>
-                                        Para enviar uma pergunta, <button type="submit" > faça seu login </button>
-                                    </span>  
-                                ) 
-                            }
-                            <Button 
-                                type="submit"
-                                className="purple"
-                                disabled={!user}
-                            > 
-                                Enviar Pergunta 
-                            </Button>
-                        </FooterForm>
-                    </Forms>
-                </Wrapper>
-
-            </div>
+                <Forms onSubmit={handleSendQuestion}>
+                    <textarea 
+                        placeholder='O que você quer perguntar? '
+                        value={newQuestion}
+                        onChange={event => setNewQuestion(event.target.value)}
+                    />
+                    <FooterForm>
+                        { user ? 
+                            (
+                                <UserInfo>
+                                    <div style={{borderRadius: '50%', overflow: 'hidden'}}>
+                                        <Image 
+                                            src={user.avatar} 
+                                            alt={user.name}
+                                            width={32}
+                                            height={32}
+                                        />
+                                    </div>
+                                    <span>{user.name}</span>
+                                </UserInfo>
+                            ) : 
+                            (
+                                <span>
+                                    Para enviar uma pergunta, <button type="submit" > faça seu login </button>
+                                </span>  
+                            ) 
+                        }
+                        <Button 
+                            type="submit"
+                            disabled={!user}
+                        > 
+                            Enviar Pergunta 
+                        </Button>
+                    </FooterForm>
+                </Forms>
+                
+                <QuestionList>
+                    {questions.map(question => {
+                        return (
+                            <Question 
+                                key={question.id} 
+                                content={question.content} 
+                                author={question.author.name} 
+                                avatar={question.author.avatar}
+                            />
+                        )                                 
+                    })}
+                </QuestionList>
+            </Wrapper>
         </>
     )
 }
