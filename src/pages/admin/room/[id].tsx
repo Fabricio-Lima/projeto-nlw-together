fd18d1b9294d9d67f1b0f9c6c0b8b76673981a53
Fabricio Lima <fabriciolc61@gmail.com>
2022-01-31 23:26:42 -0300

    adiciona listar e remover pergunta e deletar sala

--- src/pages/admin/room/[id].tsx
+++ src/pages/admin/room/[id].tsx
@@ -2,56 +2,44 @@ import { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
 import { useRouter } from "next/router"
-import { FormEvent, useState } from "react"
+import { useState } from "react"
 import logo from '../../../assets/images/logo.svg'
-import { Button, Question, RoomCode } from '../../../components/index'
+import { Button, Question, RoomCode, Modal } from '../../../components/index'
 import { useAuth } from "../../../context/auth.context"
 import useRoom from "../../../hooks/useRoom"
 import { database } from "../../../services/firebase"
 import 
-{   ButtonsNavBar,
+{
+    ButtonsModal,
+    ButtonsNavBar,
     Container, 
     Content, 
+    ContentModal, 
     Header, 
     QuestionList, 
-    Wrapper 
+    Wrapper, 
+    WrapperModal
 } from "./styles"
 
 const Room: NextPage = () => {
     const router = useRouter()
     const roomId = router.query.id
-
     const { user } = useAuth()
     const { questions, title } = useRoom(String (roomId))
-
-    const [ newQuestion, setNewQuestion ] = useState('')
-
-    const handleSendQuestion = async (event: FormEvent) => {
-        event.preventDefault()
-
-        if(newQuestion.trim() === '') {
-            return
-        }
-
-        if(!user) {
-            throw new Error('You must be logged in')
-        }
-
-        const question = {
-            content: newQuestion,
-            author: {
-                name: user?.name,
-                avatar: user.avatar
-            },
-            isHighlighted: false,
-            isAnswered: false
-        }
-
-        await database.ref(`rooms/${roomId}/questions`).push(question)
-
-        setNewQuestion('')
-    }
+    const [ modalIsOpen, setModalIsOpen ] = useState(false)
     
+    const handleEndRoom = async () => {
+        await database.ref(`rooms/${roomId}`).update({
+            endAt: new Date()
+        })
+        
+        await router.push('/')
+    }
+
+    const handleDeleteQuestion = async (questionId: string) => {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+        setModalIsOpen(false)
+    }
 
     return (
         <>
@@ -68,14 +56,19 @@ const Room: NextPage = () => {
                     />
                     <ButtonsNavBar>
                         <RoomCode code={ String(roomId) }/>
-                        <Button isOutlined> Encerrar sala </Button>
+                        <Button 
+                            isOutlined
+                            onClick={handleEndRoom}
+                        > 
+                            Encerrar sala 
+                        </Button>
                     </ButtonsNavBar>
                 </Content>
             </Header>
 
             <Wrapper>
                 <Container>
-                    <h1> Sala {title} </h1>
+                    <h1> {title} </h1>
                     {questions.length > 0 && 
                     <span>{questions.length} pergunta(s)</span>}
                 </Container>
@@ -88,7 +81,101 @@ const Room: NextPage = () => {
                                 content={question.content} 
                                 author={question.author.name} 
                                 avatar={question.author.avatar}
-                            />
+                            >
+                                <button 
+                                    type="button" 
+                                    onClick={() => setModalIsOpen(true)}
+                                >
+                                    <svg 
+                                        width="24" 
+                                        height="24" 
+                                        viewBox="0 0 24 24" 
+                                        fill="none" 
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path 
+                                            d="M3 5.99988H5H21" 
+                                            stroke="#737380" 
+                                            strokeWidth="1.5" 
+                                            strokeLinecap="round" 
+                                            strokeLinejoin="round"
+                                        />
+                                        <path 
+                                            d="M8 5.99988V3.99988C8 3.46944 8.21071 
+                                            2.96074 8.58579 2.58566C8.96086 2.21059 
+                                            9.46957 1.99988 10 1.99988H14C14.5304 
+                                            1.99988 15.0391 2.21059 15.4142 2.58566C15.7893 
+                                            2.96074 16 3.46944 16 3.99988V5.99988M19 
+                                            5.99988V19.9999C19 20.5303 18.7893 21.039 
+                                            18.4142 21.4141C18.0391 21.7892 17.5304 
+                                            21.9999 17 21.9999H7C6.46957 21.9999 5.96086 
+                                            21.7892 5.58579 21.4141C5.21071 21.039 5 
+                                            20.5303 5 19.9999V5.99988H19Z" 
+                                            stroke="#737380" 
+                                            strokeWidth="1.5" 
+                                            strokeLinecap="round" 
+                                            strokeLinejoin="round"
+                                        />
+                                    </svg>
+                                </button>
+                                <Modal status={modalIsOpen}>                                        
+                                    <ContentModal>
+                                        <svg 
+                                            width="50" 
+                                            height="100%" 
+                                            viewBox="0 0 24 24" 
+                                            fill="none" 
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path 
+                                                d="M3 5.99988H5H21" 
+                                                stroke="#737380" 
+                                                strokeWidth="2" 
+                                                strokeLinecap="round" 
+                                                strokeLinejoin="round"
+                                            />
+                                            <path 
+                                                d="M8 5.99988V3.99988C8 3.46944 8.21071 
+                                                2.96074 8.58579 2.58566C8.96086 2.21059 
+                                                9.46957 1.99988 10 1.99988H14C14.5304 
+                                                1.99988 15.0391 2.21059 15.4142 2.58566C15.7893 
+                                                2.96074 16 3.46944 16 3.99988V5.99988M19 
+                                                5.99988V19.9999C19 20.5303 18.7893 21.039 
+                                                18.4142 21.4141C18.0391 21.7892 17.5304 
+                                                21.9999 17 21.9999H7C6.46957 21.9999 5.96086 
+                                                21.7892 5.58579 21.4141C5.21071 21.039 5 
+                                                20.5303 5 19.9999V5.99988H19Z" 
+                                                stroke="#737380" 
+                                                strokeWidth="2" 
+                                                strokeLinecap="round" 
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                        <WrapperModal>
+                                            <h1>
+                                                Excluir pergunta
+                                            </h1>
+                                            <span>
+                                                Tem certeza que você deseja excluir esta pergunta?
+                                            </span>
+                                        </WrapperModal>
+
+                                        <ButtonsModal>
+                                            <Button
+                                                type="button"
+                                                onClick={() => setModalIsOpen(false)} 
+                                            > 
+                                                Cancelar 
+                                            </Button>
+                                            <Button 
+                                                onClick={() => handleDeleteQuestion(question.id)}
+                                            >
+                                                Sim, excluir
+                                            </Button>
+                                        </ButtonsModal>
+                                    </ContentModal>
+                                </Modal>
+                            </Question>
                         )                                 
                     })}
                 </QuestionList>
--- src/pages/admin/room/styles.ts
+++ src/pages/admin/room/styles.ts
@@ -52,3 +52,52 @@ export const ButtonsNavBar = styled.div`
         height: 40px;
     }
 `;
+
+export const ContentModal = styled.div`
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 30px;
+
+    svg path {
+        stroke: ${( props ) => props.theme.colors.secundary};
+    }
+`;
+
+export const WrapperModal = styled.div`
+    display: grid;
+    gap: 14px;
+
+    > h1 {
+        font-family: ${(props) => props.theme.fonts.secundaryFont};
+        font-weight: bold;
+        font-size: 24px;
+        line-height: 34px;  
+        color: #29292e;
+    }
+
+    > span {
+        font-family: ${(props) => props.theme.fonts.primaryFont};
+        font-size: 16px;
+        line-height: 26px;
+        color: #737380;
+    }
+`;
+
+export const ButtonsModal = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+
+    > button {
+        background: #dbdcdd;
+        color: #737380;
+
+        & + button {
+            background: #e73f5d;
+            color: ${(props) => props.theme.colors.background};
+        }
+    }
+`;  
